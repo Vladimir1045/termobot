ecab29c09a231a9a9e3542429c5417f24fb2084e
Sergio Keler <sergio@keler.net>
2020-11-09 16:45:33 +0300

    Более праввильно сделаны интервалы.

--- t.py
+++ t.py
@@ -4,11 +4,25 @@ import logging
 import traceback
 from bot_config import Token, API_Token
 
+intervals = [
+    {'from': None, 'to': 10, 'text': 'Сейчас ппц холодно, одевайся как танк!'},
+    {'from': 10, 'to': 20, 'text': 'Сейчас холодно, оденься потеплее.'},
+    {'from': 20, 'to': None, 'text': 'Температура норм, надевай что угодно.'},
+]
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 log = logging.getLogger(__name__)
 owm = pyowm.OWM(API_Token)
 mgr = owm.weather_manager()
 bot = telebot.TeleBot(Token)
+
+
+def get_advise(temp):
+    rc = 'Мне нечего сказать.'
+    for x in intervals:
+        if (x['from'] is None or temp >= x['from']) and (x['to'] is None or temp < x['to']):
+            return x['text']
+    return rc
 
 
 @bot.message_handler(content_types=['text'])
@@ -23,14 +37,10 @@ def send_echo(message):
         log.error(f'Ошибка {e}')
         bot.send_message(message.chat.id, f'Не могу сказать тк {e}')
         return
-    answer = f'В городе {message.text} сейчас {weather.detailed_status}\nТемпература сейчас в районе {temp}\n\n'
-    if temp < 10:
-        answer += "Сейчас ппц холодно, одевайся как танк!"
-    elif temp < 20:
-        answer += "сейчас холодно, оденься потеплее."
-    elif temp > 20:
-        answer += "Температура норм, одевай что угодно,"
-    bot.send_message(message.chat.id, answer)
+    advise = get_advise(temp)
+    bot.send_message(message.chat.id, f'В городе {message.text} сейчас {weather.detailed_status}\n'
+                                      f'Температура сейчас в районе {temp}\n\n{advise}')
+
 
 log.info('Начали')
 bot.polling(none_stop=True)   # your code goes here# your code goes here
